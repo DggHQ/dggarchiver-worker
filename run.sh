d6cf00c153c24838ae8d0363dc2be66e8a8df272
vyneer <vyn33r@gmail.com>
2023-03-19 19:56:58 +0300

    fix: add prefix to avoid filenames starting w dash

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 # start downloading strim
-./ytarchive -o '/videos/%(id)s' "https://youtu.be/$LIVESTREAM_ID" 720p/720p60/480p/360p/best
+./ytarchive -o '/videos/youtube_%(id)s' "https://youtu.be/$LIVESTREAM_ID" 720p/720p60/480p/360p/best
 
 # start processing
-./dggarchiver-worker /videos/"$LIVESTREAM_ID".mp4
+./dggarchiver-worker /videos/youtube_"$LIVESTREAM_ID".mp4
