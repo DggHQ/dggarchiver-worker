75a32a3cb0584d0dcb380c08290a2eccc196a357
vyneer <vyn33r@gmail.com>
2023-04-15 11:50:20 +0300

    feat: add a second m3u8 downloader for kick

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,26 @@
 FROM golang:alpine as builder
+ARG M3U8DL_PLATFORM='linux-x64'
 ARG TARGETARCH
 LABEL builder=true multistage_tag="dggarchiver-worker-builder"
 WORKDIR /app
 COPY . .
+RUN apk add --no-cache curl jq
+RUN if [ "$TARGETARCH" = "arm64" ] ; then export M3U8DL_PLATFORM='linux-arm64' ; fi &&\ 
+	curl https://api.github.com/repos/nilaoda/N_m3u8DL-RE/releases/latest\ 
+	| jq -r ".assets[] | select(.browser_download_url | contains(\"${M3U8DL_PLATFORM}\")) | .browser_download_url"\ 
+	| xargs wget -O m3u8dl.tar.gz\ 
+	&& tar zxf m3u8dl.tar.gz --strip-components 1\ 
+	&& rm m3u8dl.tar.gz\ 
+	&& chmod +x ./N_m3u8DL-RE
 RUN GOOS=linux GOARCH=${TARGETARCH} go install github.com/Kethsar/ytarchive@v0.3.2
-RUN GOOS=linux GOARCH=${TARGETARCH} go build
+RUN GOOS=linux GOARCH=${TARGETARCH} go build -v
 
 FROM python:alpine3.17
 WORKDIR /app
 COPY --from=builder /app/dggarchiver-worker .
 COPY --from=builder /app/run.sh .
-COPY --from=builder /go/bin/ytarchive .
+COPY --from=builder /go/bin/ytarchive /usr/bin/
+COPY --from=builder /app/N_m3u8DL-RE /usr/bin/
 RUN apk add --no-cache bash ffmpeg
 RUN pip install -U yt-dlp
 RUN chmod +x ./run.sh
--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] LIVESTREAM_INFO: $LIVESTREAM_INFO"
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] LIVESTREAM_ID: $LIVESTREAM_ID"
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] LIVESTREAM_URL: $LIVESTREAM_URL"
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] LIVESTREAM_PLATFORM: $LIVESTREAM_PLATFORM"
+if [ "$LIVESTREAM_PLATFORM" = "kick" ]; then
+	echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] KICK_DOWNLOADER: $KICK_DOWNLOADER"
+fi
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] NATS_HOST: $NATS_HOST"
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] NATS_TOPIC: $NATS_TOPIC"
 echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] VERBOSE: $VERBOSE"
@@ -13,17 +16,25 @@ set -Eeuo pipefail
 case "$LIVESTREAM_PLATFORM" in
 	"youtube" )
 		echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] [YT] Recording $LIVESTREAM_ID with ytarchive..."
-		./ytarchive -o "/videos/${LIVESTREAM_PLATFORM}_%(id)s" "$LIVESTREAM_URL" 720p/720p60/480p/360p/best
+		ytarchive -o "/videos/${LIVESTREAM_PLATFORM}_%(id)s" "$LIVESTREAM_URL" 720p/720p60/480p/360p/best
 		;;
 	"rumble" )
 		echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] [Rumble] Recording $LIVESTREAM_ID with yt-dlp..."
 		yt-dlp -f 'best[height=720][fps=30] / best[height=720] / best[height=480] / best[height=360] / best' -o "/videos/${LIVESTREAM_PLATFORM}_%(id)s.%(ext)s" "$LIVESTREAM_URL"
 		;;
 	"kick" )
-		echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] [Kick] Recording $LIVESTREAM_ID with yt-dlp..."
-		yt-dlp --downloader ffmpeg --hls-use-mpegts -f 'best[height=720][fps=30] / best[height=720] / best[height=480] / best[height=360] / best' -o "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.%(ext)s" "$LIVESTREAM_URL"
-		ffmpeg -y -loglevel "repeat+info" -i "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.mp4" -map 0 -dn -ignore_unknown -c copy -f mp4 "-bsf:a" aac_adtstoasc -movflags "+faststart" "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}.mp4"
-		rm "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.mp4"
+		echo "[$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] [Kick] Recording $LIVESTREAM_ID with ${KICK_DOWNLOADER:=yt-dlp}..."
+		export TMP_EXTENSION='mp4'
+		if [ "${KICK_DOWNLOADER}" = "yt-dlp" ]; then
+			yt-dlp --downloader ffmpeg --hls-use-mpegts -f 'best[height=720][fps=30] / best[height=720] / best[height=480] / best[height=360] / best' -o "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.%(ext)s" "$LIVESTREAM_URL"
+		elif [ "${KICK_DOWNLOADER}" = "N_m3u8DL-RE" ]; then
+			TMP_EXTENSION='ts'
+			./N_m3u8DL-RE "$LIVESTREAM_URL" --save-dir /videos/ --save-name "${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp" -sv res="720|480|360" -M format=mp4 --live-real-time-merge --live-pipe-mux --live-keep-segments=false
+		else
+			yt-dlp --downloader ffmpeg --hls-use-mpegts -f 'best[height=720][fps=30] / best[height=720] / best[height=480] / best[height=360] / best' -o "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.%(ext)s" "$LIVESTREAM_URL"
+		fi
+		ffmpeg -y -loglevel "repeat+info" -i "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.${TMP_EXTENSION}" -map 0 -dn -ignore_unknown -c copy -f mp4 "-bsf:a" aac_adtstoasc -movflags "+faststart" "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}.mp4"
+		rm "/videos/${LIVESTREAM_PLATFORM}_${LIVESTREAM_ID}_temp.${TMP_EXTENSION}"
 		;;
 esac
 
