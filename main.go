20fb2d377ed31988877e99c5fb24297850abdeb4
vyneer <vyn33r@gmail.com>
2025-05-03 03:49:32 +0300

    feat: dump vod data into a json file to allow editing it before processing the vod

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	log "github.com/DggHQ/dggarchiver-logger"
+	dggarchivermodel "github.com/DggHQ/dggarchiver-model"
 	"github.com/DggHQ/dggarchiver-worker/config"
 	"github.com/DggHQ/dggarchiver-worker/ffmpeg"
 )
@@ -39,39 +40,49 @@ func main() {
 
 	var path string
 
-	if len(os.Args) > 1 {
-		path = os.Args[1]
+	if len(os.Args) > 2 {
+		path = os.Args[2]
 	} else {
 		log.Fatalf("No path to video provided")
 	}
 
-	cfg.VOD.Path = path
+	vodBytes, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("Error reading the VOD info: %s", err)
+	}
 
-	log.Infof("Downloaded VOD with ID %s: %s", cfg.VOD.VID, cfg.VOD)
+	var vod dggarchivermodel.VOD
+	err = json.Unmarshal([]byte(vodBytes), &vod)
+	if err != nil {
+		log.Fatalf("Error unmarshalling the VOD info: %s", err)
+	}
+
+	vod.Path = path
+
+	log.Infof("Downloaded VOD with ID %s: %s", vod.VID, vod)
 
 	videoInfo := ffmpeg.GetVideoInfo(path)
 
-	cfg.VOD.Duration = int(videoInfo.Format.DurationSeconds)
+	vod.Duration = int(videoInfo.Format.DurationSeconds)
 
-	log.Infof("Added duration to VOD with ID %s: %ds", cfg.VOD.VID, cfg.VOD.Duration)
+	log.Infof("Added duration to VOD with ID %s: %ds", vod.VID, vod.Duration)
 
-	cfg.VOD.ThumbnailPath = ffmpeg.SaveFrameAsThumbnail(path, (cfg.VOD.Duration)/2, cfg.VOD.Thumbnail)
+	vod.ThumbnailPath = ffmpeg.SaveFrameAsThumbnail(path, (vod.Duration)/2, vod.Thumbnail)
 
-	var err error
-	cfg.VOD.EndTime, err = calculateEndTime(cfg.VOD.StartTime, cfg.VOD.Duration)
+	vod.EndTime, err = calculateEndTime(vod.StartTime, vod.Duration)
 	if err != nil {
-		log.Fatalf("Wasn't able to calculate end time for VOD with ID %s (dur: %d): %v", cfg.VOD.VID, cfg.VOD.Duration, err)
+		log.Fatalf("Wasn't able to calculate end time for VOD with ID %s (dur: %d): %v", vod.VID, vod.Duration, err)
 	}
 
-	bytes, err := json.Marshal(cfg.VOD)
+	bytes, err := json.Marshal(vod)
 	if err != nil {
-		log.Fatalf("Couldn't marshal VOD with ID %s into a JSON object: %v", cfg.VOD.VID, err)
+		log.Fatalf("Couldn't marshal VOD with ID %s into a JSON object: %v", vod.VID, err)
 	}
 
 	fmt.Println(string(bytes))
 
 	if err := cfg.NATSConfig.NatsConnection.Publish(fmt.Sprintf("%s.upload", cfg.NATSConfig.Topic), bytes); err != nil {
-		log.Errorf("Wasn't able to send message with VOD with ID %s: %v", cfg.VOD.VID, err)
+		log.Errorf("Wasn't able to send message with VOD with ID %s: %v", vod.VID, err)
 	}
 	cfg.NATSConfig.NatsConnection.Close()
 }
